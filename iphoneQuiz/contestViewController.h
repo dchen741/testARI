28b2b19e7762153238c61bdb5be2a7507b9c3cba
dchen741 <dchen741@gmail.com>
2014-09-02 02:22:36 -0400

    Added contest options

---
+++ iphoneQuiz/contestViewController.h
@@ -0,0 +1,14 @@
+//
+//  contestViewController.h
+//  iphoneQuiz
+//
+//  Created by Dennis Chen on 9/2/14.
+//  Copyright (c) 2014 Ivan Seto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+
+@interface contestViewController : NSObject
+
+@end
---
+++ iphoneQuiz/contestViewController.m
@@ -0,0 +1,19 @@
+//
+//  contestViewController.m
+//  iphoneQuiz
+//
+//  Created by Dennis Chen on 9/2/14.
+//  Copyright (c) 2014 Ivan Seto. All rights reserved.
+//
+
+#import "contestViewController.h"
+#import "AppDelegate.h"
+#import <Parse/Parse.h>
+
+@interface contestViewController ()
+
+@end
+
+@implementation contestViewController
+
+@end
